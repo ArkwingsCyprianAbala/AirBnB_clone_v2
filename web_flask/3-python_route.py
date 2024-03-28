7179de24b67b2f63f4718126b74ba17d539de34c
Arkwings Cyprian Abala <arkwingscyprianabala@gmail.com>
2024-03-27 18:49:09 -0700

    Web framework

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""starting a Flask Web Application """
+""" Starting a Flask Web Application """
 from flask import Flask
 app = Flask(__name__)
 
@@ -9,18 +9,21 @@ def hello_hbnb():
     """ Prints a Message when / is called """
     return 'Hello HBNB!'
 
-
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Prints a Message when /hbnb is called """
     return 'HBNB!'
-
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     """ Prints a Message when /c is called """
     return "C " + text.replace('_', ' ')
 
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
 
 if __name__ == "__main__":
     """ Main Function """
