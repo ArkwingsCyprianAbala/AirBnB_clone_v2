7f44b69cb48a9a7035716ebb42fdb9285ca182d8
Arkwings Cyprian Abala <arkwingscyprianabala@gmail.com>
2024-03-27 19:08:43 -0700

    flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -9,15 +9,18 @@ def hello_hbnb():
     """ Prints a Message when / is called """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Prints a Message when /hbnb is called """
     return 'HBNB!'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     """ Prints a Message when /c is called """
     return "C " + text.replace('_', ' ')
+
 
 if __name__ == "__main__":
     """ Main Function """
