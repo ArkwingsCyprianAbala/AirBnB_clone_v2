b3a22122284b215514b8100809f5e75e4c565e93
Arkwings Cyprian Abala <arkwingscyprianabala@gmail.com>
2024-03-27 19:17:14 -0700

    flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -14,7 +14,7 @@ def hello_hbnb():
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Prints a Message when /hbnb is called """
-    return 'HBNB!'
+    return 'HBNB'
 
 
 if __name__ == "__main__":
