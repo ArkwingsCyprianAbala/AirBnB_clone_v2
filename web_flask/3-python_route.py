2622a755716a1f77ff6c9feb84620c7f0e48ec2e
Arkwings Cyprian Abala <arkwingscyprianabala@gmail.com>
2024-03-27 18:59:34 -0700

    Web framework

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -9,15 +9,18 @@ def hello_hbnb():
     """ Prints a Message when / is called """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Prints a Message when /hbnb is called """
     return 'HBNB!'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     """ Prints a Message when /c is called """
     return "C " + text.replace('_', ' ')
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
